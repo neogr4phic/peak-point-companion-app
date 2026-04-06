83579809352fe1902f92cb6d3504f63ef90bf620
Neo Graphic <neographic@gmx.de>
2026-04-06 16:21:16 +0200

    changed delay to 1000ms

--- src/main.cpp
+++ src/main.cpp
@@ -15,17 +15,17 @@ void setup() {
 void loop() {
   // Rot an
   digitalWrite(LED_RED, LOW);
-  delay(500);
+  delay(1000);
   digitalWrite(LED_RED, HIGH);
 
   // Grün an
   digitalWrite(LED_GREEN, LOW);
-  delay(500);
+  delay(1000);
   digitalWrite(LED_GREEN, HIGH);
 
   // Blau an
   digitalWrite(LED_BLUE, LOW);
-  delay(500);
+  delay(1000);
   digitalWrite(LED_BLUE, HIGH);
 }
 
